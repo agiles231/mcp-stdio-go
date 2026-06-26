8a4f26b44178039c49a4a5bab2d2f38155d6f476
Andrew Giles <agiles231@gmail.com>
2026-06-26 16:33:27 -0400

    Add protocol types

---
+++ protocol/types.go
@@ -0,0 +1,88 @@
+package protocol
+
+import "encoding/json"
+
+const VERSION = "2.0"
+
+// Request is an incoming JSON-RPC message. A request with no ID is a
+// notification (the client expects no response)
+type Request struct {
+	JSONRPC string          `json:"jsonrpc"`
+	ID      json.RawMessage `json:"id,omitempty"`
+	Method  string          `json:"method"`
+	Params  json.RawMessage `json:"params,omitempty"`
+}
+
+// IsNotification reports whether this request expects no response.
+func (r *Request) IsNotification() bool {
+	return len(r.ID) == 0
+}
+
+// Response is an outgoing JSON-RPC message. Exactly one of Result or
+// Error is set
+type Response struct {
+	JSONRPC string          `json:"jsonrpc"`
+	ID      json.RawMessage `json:"id,omitempty"`
+	Result  any             `json:"result,omitempty"`
+	Error   *Error          `json:"error,omitempty"`
+}
+
+type Error struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+	Data    any    `json:"data,omitempty"`
+}
+
+func (e *Error) Error() string { return e.Message }
+
+const (
+	CodeParseError     = -32700
+	CodeInvalidRequest = -32600
+	CodeMethodNotFound = -32601
+	CodeInvalidParams  = -32602
+	CodeInternalError  = -32603
+)
+
+type InitializeParams struct {
+	ProtocolVersion string         `json:"protocolVersion"`
+	Capabilities    map[string]any `json:"capabilities"`
+	ClientInfo      Implementation `json:"clientInfo"`
+}
+
+type InitializeResult struct {
+	ProtocolVersion string         `json:"protocolVersion"`
+	Capabilities    map[string]any `json:"capabilities"`
+	ServerInfo      Implementation `json:"serverInfo"`
+}
+
+type Implementation struct {
+	Name    string `json:"name"`
+	Version string `json:"version"`
+}
+
+type ToolsListResult struct {
+	Tools []ToolDescriptor `json:"tools"`
+}
+
+type ToolDescriptor struct {
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	InputSchema any    `json:"inputSchema"`
+}
+
+// --- tools/call ---
+
+type ToolCallParams struct {
+	Name      string          `json:"name"`
+	Arguments json.RawMessage `json:"arguments"`
+}
+
+type ToolCallResult struct {
+	Content []Content `json:"content"`
+	IsError bool      `json:"isError,omitempty"`
+}
+
+type Content struct {
+	Type string `json:"type"`
+	Text string `json:"text,omitempty"`
+}
